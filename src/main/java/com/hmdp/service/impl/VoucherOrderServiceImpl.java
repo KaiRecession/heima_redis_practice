b45db6de884812d7db8f376cc551a1d46f9b6ae6
Kai <cwkjyllcf@163.com>
2022-11-20 16:14:24 +0800

    使用悲观锁的方式实现一人一单的问题，但是感觉会有很多问题

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -7,8 +7,10 @@ import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IVoucherService;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,7 +34,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private RedisIdWorker redisIdWorker;
 
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
         if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
@@ -44,26 +45,46 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (voucher.getStock() < 1) {
             return Result.fail("库存不足");
         }
-        // set stock = stock - 1 where id = 10 and stock = 100(刚开始查到的值)，这句话能原子
-        // mysql有锁，有点复杂，以后看看
-        boolean success = seckillVoucherService.update()
-                .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).gt("stock", 0).update();
-        if (!success) {
-            return Result.fail("库存不足");
-        }
-        VoucherOrder voucherOrder = new VoucherOrder();
-        // 生成秒杀订单表的id
-        long orderId = redisIdWorker.nextId("order");
-        // 写入id（主键）
-        voucherOrder.setId(orderId);
         Long userId = UserHolder.getUser().getId();
-        // 写入用户ID
-        voucherOrder.setUserId(userId);
-        // 写入优惠券的id
-        voucherOrder.setVoucherId(voucherId);
-        save(voucherOrder);
+        // 要让锁的范围大于事务的范围
+        synchronized (userId.toString().intern()) {
+            // spring事务的生成是spring对当前对象生成了一个代理对象，所以需要拿到事务对象，又是一个需要研究的问题
+            IVoucherOrderService proxy = (IVoucherOrderService)AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
 
-        return Result.ok(orderId);
+    // 如果直接对方法加锁，当方法执行完，锁释放了，事务只是提交了，不一定执行完毕
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        // 一人一单的解决
+        Long userId = UserHolder.getUser().getId();
+        // 对userID上锁
+
+            Integer count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+            if (count > 0) {
+                return Result.fail("你tm已经买过来");
+            }
+            // set stock = stock - 1 where id = 10 and stock = 100(刚开始查到的值)，这句话能原子
+            // mysql有锁，有点复杂，以后看看
+            boolean success = seckillVoucherService.update()
+                    .setSql("stock = stock - 1")
+                    .eq("voucher_id", voucherId).gt("stock", 0).update();
+            if (!success) {
+                return Result.fail("库存不足");
+            }
+            VoucherOrder voucherOrder = new VoucherOrder();
+            // 生成秒杀订单表的id
+            long orderId = redisIdWorker.nextId("order");
+            // 写入id（主键）
+            voucherOrder.setId(orderId);
+            // 写入用户ID
+            voucherOrder.setUserId(userId);
+            // 写入优惠券的id
+            voucherOrder.setVoucherId(voucherId);
+            save(voucherOrder);
+            return Result.ok(orderId);
+
+
     }
 }
