ba9f947696e34749f407abbb9800774833154236
Kai <cwkjyllcf@163.com>
2022-11-20 15:31:50 +0800

    解决超卖问题

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -38,6 +38,7 @@ class Solution {
 
     }
     public ListNode sortList(ListNode head) {
+        LinkedList<Integer> integers = new LinkedList<>();
         return sortList(head, null);
 
     }
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +25,45 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
 
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            return Result.fail("秒杀尚未开始");
+        }
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("秒杀已经结束");
+        }
+        if (voucher.getStock() < 1) {
+            return Result.fail("库存不足");
+        }
+        // set stock = stock - 1 where id = 10 and stock = 100(刚开始查到的值)，这句话能原子
+        // mysql有锁，有点复杂，以后看看
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId).gt("stock", 0).update();
+        if (!success) {
+            return Result.fail("库存不足");
+        }
+        VoucherOrder voucherOrder = new VoucherOrder();
+        // 生成秒杀订单表的id
+        long orderId = redisIdWorker.nextId("order");
+        // 写入id（主键）
+        voucherOrder.setId(orderId);
+        Long userId = UserHolder.getUser().getId();
+        // 写入用户ID
+        voucherOrder.setUserId(userId);
+        // 写入优惠券的id
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+
+        return Result.ok(orderId);
+    }
 }
